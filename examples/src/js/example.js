fd8cbd3493b4f2735d804b9d0aae2b3cb29e844b
Yifeng <f15gdsy@gmail.com>
2016-03-30 14:59:20 +0800

    Updated example.

--- examples/src/js/example.js
+++ examples/src/js/example.js
@@ -5,12 +5,20 @@ const ws = new WebSight(el, {
   partially: true,
 });
 
-ws.on('come-out-from-top', () => {
-  console.log('come out from top');
-}).on('come-out-from-bottom', () => {
-  console.log('come out from bottom');
+ws.on('appear-from-top', () => {
+  console.log('appear from top');
+}).on('appear-from-bottom', () => {
+  console.log('appear from bottom');
 }).on('visible', () => {
   console.log('visible');
 }).on('invisible', () => {
   console.log('invisible');
+}).on('appear', () => {
+  console.log('appear');
+}).on('disappear', () => {
+  console.log('disappear');
+}).on('disappear-from-top', () => {
+  console.log('disappear from top');
+}).on('disappear-from-bottom', () => {
+  console.log('disappear from bottom');
 });
